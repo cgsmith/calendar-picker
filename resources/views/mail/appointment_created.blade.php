5c3d1ae14a7e4146eb181525ede85120c1ee3f43
Chris Smith <chrissmith@mount7.com>
2024-07-01 13:20:53 +0200

    add appointment created email to send to customer

---
+++ resources/views/mail/appointment_created.blade.php
@@ -0,0 +1,24 @@
+#Subject: Mount7 Werkstatt Reparatur {date} - Important Info.
+
+Hallo {user},
+
+Vielen Dank für deine Termin.
+
+# Important to confirm
+Sehr gerne kannst du das Rad zwischen unseren Öffnungszeiten (Mo-Fr 12-18:30, Sa 10-14) vorbeibringen. Bitte bringe das Rad am Tag vorher während unserer Öffnungszeiten vorbei, damit wir das Rad pünktlich hier haben.
+
+### TERMIN
+describe appointment here to customer
+
+## required
+Die aktuelle Reparaturzeit beträgt 1-2 Werktage. Bitte beachte, dass wir diesen Zeitraum nur einhalten können, sofern alle nötigen Ersatzteile und Verschleißteile bei uns lagernd sind und der Reparaturumfang nicht außerplanmäßig erhöht wird. Wenn das nicht der Fall ist, kann sich die Bearbeitungszeit verlängern, dann geben wir dir aber natürlich rechtzeitig Bescheid.
+
+Alle weiteren Details kannst du sehr gerne vor Ort mit der Werkstatt besprechen, dann haben wir einen besseren Eindruck von dem Rad und was gemacht werden muss.
+Wenn dein Rad ein Leasingrad ist, bringe bitte die entsprechenden Unterlagen mit und setze uns bei der Annahme des Rades darüber in Kenntnis.
+
+## added this
+Feel free to reply directly to this email if you have any other questions!
+
+Viele Grüße aus Freiburg
+
+Dein Mount7 Team
