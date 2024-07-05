642e075ddb22c52aa1acc37d3b7545962a71e43c
Chris Smith <chrissmith@mount7.com>
2024-07-05 12:38:01 +0200

    thank you update for dark mode

--- resources/views/appointment/thankyou.blade.php
+++ resources/views/appointment/thankyou.blade.php
@@ -30,10 +30,10 @@
         </script>
     @endpush
 
-    <div class="bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
+    <div class="dark:bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
         <div class="mx-auto max-w-2xl text-center">
-            <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">{{__('Appointment Booked!')}}</h2>
-            <p class="mt-6 text-lg leading-8 text-gray-300">{{ __('Thank you! We look forward to meeting with you. We will send you an email with the appointment details. Please contact us if you have any questions.') }}</p>
+            <h2 class="text-4xl font-bold tracking-tight dark:text-white sm:text-6xl">{{__('Appointment Booked!')}}</h2>
+            <p class="mt-6 text-lg leading-8 dark:text-gray-300">{{ __('Thank you! We look forward to meeting with you. We will send you an email with the appointment details. Please contact us if you have any questions.') }}</p>
         </div>
     </div>
 
