7a90bc1f4d20099ef9286ce65616461c8909c4ea
Chris Smith <chrissmith@mount7.com>
2024-08-26 10:34:00 +0200

    move allowed IPs to the .env file

--- app/Http/Middleware/BlockIp.php
+++ app/Http/Middleware/BlockIp.php
@@ -5,23 +5,49 @@ declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
 class BlockIp
 {
-    // set IP addresses
-    public $allowIps = ['94.134.138.4', '103.204.129.148', '127.0.0.1', '::1'];
+    /**
+     * Set IP addresses in ALLOWED_IPS env variable
+     */
+    public array $allowIps = [];
+
+    public function __construct()
+    {
+        // Explode list of IPs into an array and perform a trim on them
+        $allowedIPs = explode(separator: ',', string: getenv('ALLOWED_IPS'));
+        array_walk($allowedIPs, function (&$value) {
+            $value = str(trim($value));
+        });
+
+        $this->allowIps = $allowedIPs;
+    }
+
+    /**
+     * Add the IP address to the error message if app.debug is true
+     *
+     * @param Request $request
+     * @return string
+     */
+    private function addIpToDebug(Request $request): string
+    {
+        return config('app.debug') ? ' IP ' . $request->ip() : '';
+    }
 
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        if (! in_array($request->ip(), $this->allowIps)) {
+        if (!in_array($request->ip(), $this->allowIps)) {
             return response()->json([
-                'message' => "You don't have permission to access this website.",
+                'message' => "You don't have permission to access this page." . $this->addIpToDebug($request),
             ], 401);
         }
 
